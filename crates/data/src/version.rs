94abae4c4ddcc681b25ec1e412e6366be61aa5dd
Borja Castellano <borjacastellano1@gmail.com>
2025-05-04 18:36:07 +0200

    removed tagged versions. only SemVer

--- crates/data/src/version.rs
+++ crates/data/src/version.rs
@@ -2,51 +2,11 @@ use serde::{Deserialize, Deserializer, Serialize};
 use std::cmp::Ordering;
 use std::fmt::{Display, Formatter};
 
-#[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Debug)]
-pub enum VersionWeight {
-    Snapshot = 0,
-    Alpha = 1,
-    Beta = 2,
-    Rc = 3,
-    Prerelease = 4,
-    Release = 5,
-}
-
-impl TryFrom<String> for VersionWeight {
-    type Error = crate::Error;
-
-    fn try_from(value: String) -> Result<Self, Self::Error> {
-        match value.as_str() {
-            "SNAPSHOT" => Ok(VersionWeight::Snapshot),
-            "ALPHA" => Ok(VersionWeight::Alpha),
-            "BETA" => Ok(VersionWeight::Beta),
-            "RC" => Ok(VersionWeight::Rc),
-            "PRERELEASE" => Ok(VersionWeight::Prerelease),
-            "RELEASE" => Ok(VersionWeight::Release),
-            _ => Err(crate::Error::InvalidStringFormat("Invalid version weight")),
-        }
-    }
-}
-
-impl Display for VersionWeight {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            VersionWeight::Snapshot => write!(f, "SNAPSHOT"),
-            VersionWeight::Alpha => write!(f, "ALPHA"),
-            VersionWeight::Beta => write!(f, "BETA"),
-            VersionWeight::Rc => write!(f, "RC"),
-            VersionWeight::Prerelease => write!(f, "PRERELEASE"),
-            VersionWeight::Release => write!(f, "RELEASE"),
-        }
-    }
-}
-
 #[derive(Eq, Clone, Default)]
 pub struct Version {
     major: u32,
     minor: u32,
     patch: u32,
-    weight: Option<VersionWeight>,
 }
 
 impl Version {
@@ -59,21 +19,7 @@ impl TryFrom<String> for Version {
     type Error = crate::Error;
 
     fn try_from(value: String) -> Result<Self, Self::Error> {
-        let version_splitted: Vec<&str> = value.split('-').collect();
-
-        if version_splitted.len() > 2 {
-            return Err(crate::Error::InvalidStringFormat(
-                "Version has more than one '-'",
-            ));
-        }
-
-        let weight = if version_splitted.len() == 2 {
-            Some(VersionWeight::try_from(version_splitted[1].to_string())?)
-        } else {
-            None
-        };
-
-        let numbers: Vec<&str> = version_splitted[0].split('.').collect();
+        let numbers: Vec<&str> = value.split('.').collect();
 
         if numbers.len() != 3 {
             return Err(crate::Error::InvalidStringFormat(
@@ -95,7 +41,6 @@ impl TryFrom<String> for Version {
             major,
             minor,
             patch,
-            weight,
         })
     }
 }
@@ -121,12 +66,7 @@ impl Serialize for Version {
 
 impl Display for Version {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        if self.weight.is_some() {
-            let weight = self.weight.clone().unwrap();
-            write!(f, "{}.{}.{}-{}", self.major, self.minor, self.patch, weight)
-        } else {
-            write!(f, "{}.{}.{}", self.major, self.minor, self.patch)
-        }
+        write!(f, "{}.{}.{}", self.major, self.minor, self.patch)
     }
 }
 
@@ -148,10 +88,7 @@ impl Ord for Version {
         if self.minor != other.minor { return self.minor.cmp(&other.minor) }
         if self.patch != other.patch { return self.patch.cmp(&other.patch) }
         
-        let self_weight = self.weight.clone().unwrap_or(VersionWeight::Release);
-        let other_weight = other.weight.clone().unwrap_or(VersionWeight::Release);
-        
-        self_weight.cmp(&other_weight)
+        Ordering::Equal
     }
 }
 
@@ -168,30 +105,6 @@ mod test {
         let version1 = Version::new("1.0.0".to_string()).unwrap();
         let version2 = Version::new("1.0.1".to_string()).unwrap();
         assert!(version1 < version2);
-
-        let version1 = Version::new("1.0.0".to_string()).unwrap();
-        let version2 = Version::new("1.0.0-SNAPSHOT".to_string()).unwrap();
-        assert_eq!(version1.cmp(&version2), Ordering::Greater);
-
-        let version1 = Version::new("1.0.0".to_string()).unwrap();
-        let version2 = Version::new("1.0.0-ALPHA".to_string()).unwrap();
-        assert_eq!(version1.cmp(&version2), Ordering::Greater);
-
-        let version1 = Version::new("1.0.0".to_string()).unwrap();
-        let version2 = Version::new("1.0.0-BETA".to_string()).unwrap();
-        assert_eq!(version1.cmp(&version2), Ordering::Greater);
-
-        let version1 = Version::new("1.0.0".to_string()).unwrap();
-        let version2 = Version::new("1.0.0-RC".to_string()).unwrap();
-        assert_eq!(version1.cmp(&version2), Ordering::Greater);
-
-        let version1 = Version::new("1.0.0".to_string()).unwrap();
-        let version2 = Version::new("1.0.0-PRERELEASE".to_string()).unwrap();
-        assert_eq!(version1.cmp(&version2), Ordering::Greater);
-
-        let version1 = Version::new("1.0.0".to_string()).unwrap();
-        let version2 = Version::new("1.0.0-RELEASE".to_string()).unwrap();
-        assert_eq!(version1.cmp(&version2), Ordering::Equal);
 
         let version1 = Version::new("1.0.0".to_string()).unwrap();
         let version2 = Version::new("0.1.28".to_string()).unwrap();
